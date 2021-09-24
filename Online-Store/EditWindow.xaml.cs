7dd9f53e4490286d8b82b8c38d3baa62e04fdc81
AmiraslanSumgait <emiraslaneliyev45@gmail.com>
2021-09-24 16:50:39 +0400

    Searchbar,add product logics completed

---
+++ Online-Store/EditWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Win32;
+using Online_Store.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Online_Store
+{
+    /// <summary>
+    /// Interaction logic for EditWindow.xaml
+    /// </summary>
+    public partial class EditWindow : Window, INotifyPropertyChanged
+    {
+        private Product product;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyRaised([CallerMemberName] string propertyname = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+
+        public Product Product
+        {
+            get { return product; }
+            set { product = value; OnPropertyRaised(); }
+        }
+        public EditWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void ChangeImage_Click(object sender, RoutedEventArgs e)
+        {
+            if ((ChangeImage.Content).Equals("Change Image")) 
+            {
+                OpenFileDialog openFile = new OpenFileDialog();
+                openFile.Title = "Select a picture";
+                openFile.Multiselect = false;
+                openFile.Filter = "PNG Files (*.png)|*.png|JPEG Files (*.jpeg)|*.jpeg|JPG Files (*.jpg)|*.jpg";
+                if (openFile.ShowDialog() == true)
+                {
+                    image.Source = new BitmapImage(new Uri(openFile.FileName));
+                    if (Product == null)
+                    {
+                        Product = new Product()
+                        {
+                            Name = txtBoxName.Text,
+                            Price = double.Parse(txtBoxPrice.Text)
+                        };
+                    }
+                    Product.ImagePath = openFile.FileName;
+                }
+            }
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            openFile.Multiselect = false;
+            openFile.Filter = "PNG Files (*.png)|*.png|JPEG Files (*.jpeg)|*.jpeg|JPG Files (*.jpg)|*.jpg";
+            openFile.FilterIndex = 2;
+
+            if (openFile.ShowDialog() == true)
+            {
+                image.Source = new BitmapImage(new Uri(openFile.FileName));
+            }
+        }
+    }
+}
--- Online-Store/MainWindow.xaml.cs
+++ Online-Store/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace Online_Store
     public partial class MainWindow : Window
     {
         public ObservableCollection<Product> Products { get; set; }
+        public ObservableCollection<Product> ProductsCopy { get; set; }
+        private EditWindow editWindow = new EditWindow();
         public MainWindow()
         {
             InitializeComponent();
@@ -43,9 +45,9 @@ namespace Online_Store
             },
             new Product
             {
-                Name="Iphone X",
-                 Price=1100,
-                  ImagePath="Images/iphoneX.png",
+                Name="Xiaomi Redmi 9",
+                 Price=440,
+                  ImagePath="Images/xiaomi.png",
             },
             new Product
 
@@ -68,6 +70,7 @@ namespace Online_Store
 
             }
           };
+            ProductsCopy = Products;
         }
         private void dragMe(object sender, MouseButtonEventArgs e)
         {
@@ -77,6 +80,82 @@ namespace Online_Store
             }
             catch (Exception)
             {
+            }
+        }
+
+        private void txb_watermark_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txb_watermark.Visibility = System.Windows.Visibility.Collapsed;
+            txb_main.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void txb_main_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txb_main.Text))
+            {
+                txb_watermark.Visibility = System.Windows.Visibility.Visible;
+                txb_main.Visibility = System.Windows.Visibility.Collapsed;
+            }
+        }
+
+        private void ShutDown_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ProductListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+           var  editWindow = new EditWindow();
+           editWindow.Product = (sender as ListBox).SelectedItem as Product;
+            editWindow.btn.Visibility = Visibility.Hidden;
+           editWindow.ShowDialog();
+        }
+
+        private void AddPhone_Click(object sender, RoutedEventArgs e)
+        {
+            
+            editWindow.ChangeImage.Content = "Add";
+            editWindow.Show();
+         
+            editWindow.ChangeImage.Click += ChangeImage_Click;
+        }
+
+        private void ChangeImage_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (editWindow.txtBoxName.Text == null || editWindow.txtBoxPrice.Text == null || editWindow.image.Source == null)
+            {
+                MessageBox.Show("Fill All cridentials !");
+            }
+            else
+            {
+                Product product = new Product()
+                {
+                    Name = editWindow.txtBoxName.Text,
+                    Price = Convert.ToDouble(editWindow.txtBoxPrice.Text),
+                    ImagePath = editWindow.image.Source.ToString()
+                };
+
+                Products.Add(product);
+            }
+        }
+
+        private void txb_main_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txb_main.Text.Length != 0 && txb_main.Text != "Search")
+            {
+                var searchText = txb_main.Text.ToLower();
+                List<Product> productsList = new List<Product>();
+                productsList = ProductsCopy.Where(p => p.Name.ToLower().StartsWith(searchText)).ToList();
+                ObservableCollection<Product> newList = new ObservableCollection<Product>(productsList);
+                Products = newList;
+                ProductListBox.ItemsSource = newList;
+
+            }
+            else if (txb_main.Text.Length == 0)
+            {
+                Products = ProductsCopy;
+                ProductListBox.ItemsSource = Products;
             }
         }
     }
