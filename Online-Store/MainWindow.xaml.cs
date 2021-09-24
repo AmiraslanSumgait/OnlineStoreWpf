4ab30c82cdbb297dbcea35ba56893724d58af4b1
AmiraslanSumgait <emiraslaneliyev45@gmail.com>
2021-09-24 10:10:06 +0400

    UI completed

--- Online-Store/MainWindow.xaml.cs
+++ Online-Store/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Online_Store.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,62 @@ namespace Online_Store
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Product> Products { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            Products = new ObservableCollection<Product>()
+            {
+            new Product
+            {
+                Name="Sony Xperia",
+                Price=150,
+                ImagePath="Images/soxyxperiaZ.png",
+            },
+            new Product
+            {
+                Name="Galaxy S7",
+                Price=240,
+                ImagePath="Images/galaxys7.png",
+            },
+            new Product
+            {
+                Name="Iphone X",
+                 Price=1100,
+                  ImagePath="Images/iphoneX.png",
+            },
+            new Product
+
+            {
+                Name="Iphone 11 pro",
+                 Price=2400,
+                  ImagePath="Images/iphone11pro.png",
+            },
+             new Product
+            {
+                Name="SonyXperiaL",
+                 Price=230,
+                  ImagePath="Images/SonyXperiaL.png",
+            },
+              new Product
+            {
+                Name="Iphone 8",
+                 Price=890,
+                  ImagePath="Images/iphone8.png",
+
+            }
+          };
+        }
+        private void dragMe(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }
